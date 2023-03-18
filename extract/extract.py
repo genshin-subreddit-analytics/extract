3de8bc1b89c809968e9b0d092ff7de09e7bd62b7
Lethargy4294 <Lethargy4294@proton.me>
2023-03-18 15:33:46 +0700

    feat: Get subreddit name from environment

--- extract/extract.py
+++ extract/extract.py
@@ -1,20 +1,24 @@
 import traceback
+import os
 from datetime import datetime
 
 from module.email_manager import EmailManager
 
 
 def main():
-    email_manager = EmailManager()
     try:  # Acquiring, Cleaning, and Storing Data
+        # Get SES Client
+        email_manager = EmailManager()
         # Get Status (from Database? S3?)
+        subreddit = os.environ["SUBREDDIT_NAME"]
+        start_time = "-"
         end_time = datetime.now()
-        # Send email: extraction starting
+        # Send email template: extraction starting
         email_manager.send(
             "GSA-Start",
             {
-                "subreddit": "niloumains",
-                "start_time": "-",
+                "subreddit": subreddit,
+                "start_time": start_time,
                 "end_time": end_time.strftime("%c")
             }
         )
@@ -28,20 +32,20 @@ def main():
         email_manager.send(
             "GSA-Error",
             {
-                "subreddit": "niloumains",
-                "start_time": "June 26th, 2003",
+                "subreddit": subreddit,
+                "start_time": start_time,
                 "end_time": end_time.strftime("%c"),
                 "error_trace": stacktrace
             }
         )
-        print(stacktrace)
+        print(e)
     else:  # Handle Success
         # Send email: extraction succesful, num of data parsed
         email_manager.send(
             "GSA-Complete",
             {
-                "subreddit": "niloumains",
-                "start_time": "June 26th, 2003",
+                "subreddit": subreddit,
+                "start_time": start_time,
                 "end_time": end_time.strftime("%c")
             }
         )
